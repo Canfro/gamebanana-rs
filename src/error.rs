1e23bae4493a47524a9f1a391f499ccc15d45897
Franco Dalla Via Oliveros <francodvo@gmail.com>
2026-09-07 11:30:54 -0300

    removed api folder and started over

--- src/error.rs
+++ src/error.rs
@@ -1,3 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Clone, Copy, Deserialize, Serialize)]
 pub enum Error {
     UrlError,
+    FailedToSend,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,30 @@
 use std::str::FromStr;
 
-use reqwest::Url;
+use reqwest::{Client, Url};
+use serde::{Deserialize, Serialize};
 
 use crate::error::Error;
 
 mod api_v12;
 pub(crate) mod error;
 
+#[derive(Debug, Clone)]
 pub struct GamebananaApi {
-    main_url: Url,
+    url_v12: Url,
+    client: Client,
 }
 
-impl GamebananaApi {
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub(crate) struct Response<'a, T> {
+    status: &'a str,
+    data: T,
+}
+
+impl<'a> GamebananaApi {
     pub fn new() -> Result<GamebananaApi, Error> {
         Ok(GamebananaApi {
-            main_url: Url::from_str("https://gamebanana.com/apiv12")
-                .map_err(|_| Error::UrlError)?,
+            url_v12: Url::from_str("https://gamebanana.com/apiv12").map_err(|_| Error::UrlError)?,
+            client: Client::new(),
         })
     }
 }
